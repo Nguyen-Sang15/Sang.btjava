2cf51d9ed01ba068773508ffeebbab7a80ed1024
Nguyen-Sang15 <sangnguyen151006@gmail.com>
2025-04-13 17:26:34 +0700

    Add files via upload

---
+++ BTvn3/BT1/Exercise1.java
@@ -0,0 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Exercise1 {
+public static void main(String[] args) throws IOException {
+	FileInputStream fileInputStream= new FileInputStream("C:/Users/DELL/Documents/a.txt");
+    int size=fileInputStream.available();
+    byte []buff= new byte[size];
+    fileInputStream.read(buff, 0, size);
+    String s=new String(buff);
+    System.out.println(s);
+    FileOutputStream fileOutputStream= new FileOutputStream("C:/Users/DELL/Documents/b.txt");
+    fileOutputStream.write(buff);
+    fileInputStream.close();
+    
+}
+}
